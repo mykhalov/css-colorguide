ca884d75f462456cf75c23f1f365dbe35fe4c0c3
Pavlo Mykhalov <pmykhalov@codeminders.com>
2013-09-26 00:24:30 +0300

    Major rewrite using Ember

--- js/app.js
+++ js/app.js
@@ -1,57 +1,64 @@
-/* global Handlebars, Color */
+Parser = Ember.Application.create();
 
-document.addEventListener('DOMContentLoaded', function () {
-  'use strict';
+Parser.Swatch = Ember.Object.extend({
+  color: '',
+  occurences: 0,
+  style: function () {
+    return 'background-color:' + this.get('color');
+  }.property('color'),
+  title: function () {
+    return this.get('color') + ' ×' + this.get('occurences');
+  }.property('color', 'occurences')
+});
 
-  var inputElement = document.getElementById('input');
-  var outputElement = document.getElementById('output');
-  var templateElement = document.getElementById('template');
+Parser.ApplicationController = Ember.Controller.extend({
+  actions: {
+    clear: function () {
+      this.set('inputString', '');
+    },
+    parse: function () {
+      var inputString = this.get('inputString');
 
-  var template = Handlebars.compile(templateElement.innerHTML);
+      // Define color schemes used in CSS
+      var colorSchemes = [
+        /#[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?/g,
+        /rgb\(\d{1,3},\s*\d{1,3},\s*\d{1,3}\)/g,
+        /rgba\(\d{1,3},\s*\d{1,3},\s*\d{1,3},\s*0?\.\d+\)/g,
+        /hsl\(\d{1,3},\s*\d{1,3}%,\s*\d{1,3}%\)/g,
+        /hsla\(\d{1,3},\s*\d{1,3}%,\s*\d{1,3}%,\s*0?\.\d+\)/g
+      ];
 
-  if (localStorage.input) {
-    inputElement.value = localStorage.input;
+      var matches = [];
+
+      // Match every color scheme
+      colorSchemes.forEach(function (scheme) {
+        matches = matches.concat(inputString.match(scheme) || []);
+      });
+
+      var swatches = {};
+
+      // Count occurences of each color
+      matches.forEach(function (value) {
+        var color = Color(value).rgbString();
+        swatches[color] = (swatches[color] || 0) + 1;
+      });
+
+      var swatchesArray = [];
+
+      // Create an arrays of Swatch objects
+      Object.keys(swatches).forEach(function (key) {
+        swatchesArray.push(Parser.Swatch.create({
+          color: key,
+          occurences: swatches[key]
+        }));
+      });
+
+      // Sort by occurences
+      swatchesArray.sort(function (a, b) {
+        return b.get('occurences') - a.get('occurences');
+      });
+
+      this.set('swatches', swatchesArray);
+    }
   }
-
-  inputElement.oninput = function () {
-    localStorage.input = this.value;
-  };
-
-  window.onreset = function () {
-    delete localStorage.input;
-    outputElement.innerHTML = '';
-  };
-
-  document.forms[0].onsubmit = function (e) {
-    var colorSchemes = [
-      /#[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?/g,
-      /rgb\(\d{1,3},\s*\d{1,3},\s*\d{1,3}\)/g,
-      /rgba\(\d{1,3},\s*\d{1,3},\s*\d{1,3},\s*0?\.\d+\)/g,
-      /hsl\(\d{1,3},\s*\d{1,3}%,\s*\d{1,3}%\)/g,
-      /hsla\(\d{1,3},\s*\d{1,3}%,\s*\d{1,3}%,\s*0?\.\d+\)/g
-    ];
-    var matches = [];
-
-    colorSchemes.forEach(function (scheme) {
-      matches = matches.concat(inputElement.value.match(scheme) || []);
-    });
-
-    var swatches = {};
-
-    matches.forEach(function (value) {
-      var color = Color(value).rgbString().toLowerCase();
-      swatches[color] = (swatches[color] || 0) + 1;
-    });
-
-    // TODO: Sort swatches by times used
-
-    outputElement.innerHTML = template({
-      colorsFound: Boolean(matches.length), 
-      swatches: swatches
-    });
-
-    location.href = '#result';
-
-    e.preventDefault();
-  };
 });
