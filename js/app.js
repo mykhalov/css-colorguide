05966d055489434a37b95f60b48c80eaae2deb1d
Pavlo Mykhalov <pmykhalov@codeminders.com>
2013-10-10 18:31:29 +0300

    Improved regexes

--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@
 Parser = Ember.Application.create();
 
 Parser.COLOR_SCHEMES = [
-  /#[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?/g,
-  /rgb\(\d{1,3},\s*\d{1,3},\s*\d{1,3}\)/g,
-  /rgba\(\d{1,3},\s*\d{1,3},\s*\d{1,3},\s*0?\.\d+\)/g,
-  /hsl\(\d{1,3},\s*\d{1,3}%,\s*\d{1,3}%\)/g,
-  /hsla\(\d{1,3},\s*\d{1,3}%,\s*\d{1,3}%,\s*0?\.\d+\)/g
+  /#[0-9a-f]{3}([0-9a-f]{3})?/gi,
+  /rgb\(\d{1,3},\s*\d{1,3},\s*\d{1,3}\)/gi,
+  /rgba\(\d{1,3},\s*\d{1,3},\s*\d{1,3},\s*\d*\.\d+\)/gi,
+  /hsl\(\d{1,3},\s*\d{1,3}%,\s*\d{1,3}%\)/gi,
+  /hsla\(\d{1,3},\s*\d{1,3}%,\s*\d{1,3}%,\s*\d*\.\d+\)/gi,
+  /black|silver|gr(a|e)y|white|maroon|red|purple|fuchsia|green|lime|olive|yellow|navy|blue|teal|aqua|orange|aliceblue|antiquewhite|aquamarine|azure|beige|bisque|blanchedalmond|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|darkblue|darkcyan|darkgoldenrod|darkgr(a|e)y|darkgreen|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategr(a|e)y|darkturquoise|darkviolet|deeppink|deepskyblue|dimgr(a|e)y|dodgerblue|firebrick|floralwhite|forestgreen|gainsboro|ghostwhite|gold|goldenrod|greenyellow|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgr(a|e)y|lightgreen|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategr(a|e)y|lightsteelblue|lightyellow|limegreen|linen|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|oldlace|olivedrab|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|skyblue|slateblue|slategr(a|e)y|snow|springgreen|steelblue|tan|thistle|tomato|turquoise|violet|wheat|whitesmoke|yellowgreen/gi
 ];
 
 Parser.ApplicationController = Ember.Controller.extend({
